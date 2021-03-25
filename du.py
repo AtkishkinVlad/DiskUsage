266141f5202f0104cda67b7d3a6f1cbf3a3923ba
Vladislav <vladatkishkin@gmail.com>
2021-03-25 22:19:47 +0500

    Add pre alpha ext

--- du.py
+++ du.py
@@ -1,4 +1,7 @@
 import argparse
+from glob import glob
+from sys import exit
+from os.path import getsize
 from diskusage import sort_dirs, convert_bytes, traversal
 
 def arguments_parsing():
@@ -8,17 +11,23 @@ def arguments_parsing():
                         help='Type of sorting: name - lexical, size - by size')
     parser.add_argument('-d', '--depth', type=int, default=2, help='Max depth of subdirectories')
     parser.add_argument('--reverse', default=False, action='store_true', help='Reverse sorting')
-    parser.add_argument('--ext', default=False, action='store_true', help='Show total file size by extensions')
     parser.add_argument('--noprogress', default=False, action='store_true', help='Not to show processing progress')
-    parser.add_argument('--top', default=-1, type=int, help='Select [VALUE] files from top of list')
+    parser.add_argument('--top', default=-1, type=int, help='top element by size')
     parser.add_argument('--block', default=100, type=int, help='Get files that consume [VALUE] percents of used space')
+    parser.add_argument('--ext', default=False, type=str, help='Get files with necessary size + size')
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = arguments_parsing()
-    result, maxlen = traversal(args.base_path, args.depth)
+    result, max_len = traversal(args.base_path, args.depth)
+
+    if args.ext:
+        res_list = glob(f'{args.base_path}\*.{args.ext}', recursive=True)
+        print(*res_list, convert_bytes(sum(map(getsize, res_list))), sep='\n')
+        exit()
+
     if args.top == -1:
         border = len(result)
     else:
@@ -42,18 +51,18 @@ if __name__ == '__main__':
             if remainingAmount <= 0:
                 break
         for i in output:
-            print(i.name, ' ' * (maxlen - len(i.name)), '\t', convert_bytes(i.size), '\t', str(i.time).split('.')[0])
-        print('remaining files:', ' ' * (maxlen - len('remaining files:')), '\t', convert_bytes(totalSize - qwe + remainingAmount))
+            print(i.name, ' ' * (max_len - len(i.name)), '\t', convert_bytes(i.size), '\t', str(i.time).split('.')[0])
+        print('remaining files:', ' ' * (max_len - len('remaining files:')), '\t', convert_bytes(totalSize - qwe + remainingAmount))
     elif args.sort != 'none':
         result = sort_dirs(result, args.sort, args.reverse)
         for i in result[:border]:
             name = i.name
             if i.isDir:
                 name = f'\033[96m{name}\033[0m'
-            print(name, ' ' * (maxlen - len(i.name + i.indent)), '\t', convert_bytes(i.size), '\t', str(i.time).split('.')[0])
+            print(name, ' ' * (max_len - len(i.name + i.indent)), '\t', convert_bytes(i.size), '\t', str(i.time).split('.')[0])
     else:
         for i in result:
             name = i.name
             if i.isDir:
                 name = f'\033[96m{name}\033[0m'
-            print(i.indent, name, ' ' * (maxlen - len(i.name + i.indent)), convert_bytes(i.size), '\t', str(i.time).split('.')[0])
+            print(i.indent, name, ' ' * (max_len - len(i.name + i.indent)), convert_bytes(i.size), '\t', str(i.time).split('.')[0])
